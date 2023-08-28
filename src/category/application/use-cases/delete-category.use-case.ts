461c9e9139bc03c262a01e6c941d75820d1ef22e
Fernando Soares <fndcaique@gmail.com>
2023-08-28 09:41:37 -0300

    feat: delete category use case

---
+++ src/category/application/use-cases/delete-category.use-case.ts
@@ -0,0 +1,16 @@
+import UseCase from '../../../common/application/use-case';
+import CategoryRepository from '../../domain/repository/category.repository';
+
+export default class DeleteCategoryUseCase implements UseCase<Input, Output> {
+  constructor(private categoryRepository: CategoryRepository) {}
+
+  async execute(input: Input): Promise<Output> {
+    await this.categoryRepository.delete(input.id);
+  }
+}
+
+export type Input = {
+  id: string;
+};
+
+export type Output = void;
