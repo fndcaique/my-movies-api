568bb6ca0ca4c10d9168aadc10a490dfde123407
Fernando Soares <fndcaique@gmail.com>
2023-08-28 08:22:03 -0300

    feat: update category use case

---
+++ src/category/application/use-cases/update-category.use-case.ts
@@ -0,0 +1,28 @@
+import UseCase from '../../../common/application/use-case';
+import CategoryRepository from '../../domain/repository/category.repository';
+import { CategoryOuputMappper, CategoryOutput } from '../dto/category-output';
+
+export default class UpdateCategoryUseCase implements UseCase<Input, Output> {
+  constructor(private categoryRepository: CategoryRepository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const entity = await this.categoryRepository.findById(input.id);
+    entity.update(input.name, input.description);
+    if (input.isActive === true) {
+      entity.activate();
+    } else if (input.isActive === false) {
+      entity.deactivate();
+    }
+    await this.categoryRepository.update(entity);
+    return CategoryOuputMappper.toOutput(entity);
+  }
+}
+
+export type Input = {
+  id: string;
+  name: string;
+  description?: string;
+  isActive?: boolean;
+};
+
+export type Output = CategoryOutput;
