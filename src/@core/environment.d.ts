41802a9d32941d5acdbba7047d4e6e452e83beb7
Fernando Soares <fndcaique@gmail.com>
2023-10-14 14:29:46 -0300

    refactor: import export core

--- src/@core/environment.d.ts
+++ src/@core/environment.d.ts
@@ -2,11 +2,11 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production';
-      PORT?: number;
+      PORT?: string;
       API_HOSTNAME?: string;
-      API_PORT?: number;
+      API_PORT?: string;
       DB_HOST?: string;
-      DB_PORT?: number;
+      DB_PORT?: string;
       DB_DATABASE?: string;
       DB_USERNAME?: string;
       DB_PASSWORD?: string;
@@ -16,4 +16,4 @@ declare global {
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export { };
+export {};
